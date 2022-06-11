6bc7a4ded73079e2f7fe66d2ea9ef6e5a99c34c6
soumyasomasundaran <14910227+soumyas567@users.noreply.github.com>
2022-06-11 05:43:03 +0100

    config updated

--- tweet_analyzer/config.py
+++ tweet_analyzer/config.py
@@ -3,8 +3,9 @@
 
 import streamlit as st
 
-st.secrets["CONSUMER_KEY"]
-st.secrets["CONSUMER_SECRET"]
-st.secrets["ACCESS_TOKEN"]
-st.secrets["ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY = st.secrets["CONSUMER_KEY"]
+CONSUMER_SECRET = st.secrets["CONSUMER_SECRET"]
+ACCESS_TOKEN = st.secrets["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = st.secrets["ACCESS_TOKEN_SECRET"]
+BEARER_TOKEN = st.secrets["BEARER_TOKEN"]
 
